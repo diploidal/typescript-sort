21d7e06fc11034c1c5590a7bbba9c9adaa210dd1
Diploidal <diploidal@gmail.com>
2021-11-07 21:08:42 +0100

    add implementation of character bubble sort

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([1, -19, 4, -5, 5]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('Xxaydsbljiurtom');
+const characterSorter = new Sorter(charactersCollection);
+characterSorter.sort();
+console.log(charactersCollection.data);
