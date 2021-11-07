678934447e33154d332e29bfec2d2079f07baa6d
Diploidal <diploidal@gmail.com>
2021-11-07 16:41:50 +0100

    introduction to type guard in ts

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,17 @@
-// const bubbleSortArr = [0, 10, -1, 5];
-
-// const bubbleSortFn = (array: number[]) => {
-//   for (let i = 0; i < array.length; i++) {
-//     console.log(array[i]);
-//   }
-// };
-
-// bubbleSortFn(bubbleSortArr);
-
 class Sorter {
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     const { length } = this.collection;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            const leftHand = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = leftHand;
+          }
         }
       }
     }
